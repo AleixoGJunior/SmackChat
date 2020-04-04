89f89c90a129d3d92da29ad4ed511c1b8a955132
Aleixo G Junior <aleixo.cco@gmail.com>
2020-04-04 18:37:17 -0300

    Create some initial pages and routes

---
+++ src/pages/PageChat.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-page class="flex column">
+    <q-banner class="text-center bg-grey-4">
+      User is offline.
+    </q-banner>
+    <div class="q-pa-md column col justify-end">
+      <q-chat-message
+        v-for="message in messages"
+        :key="message.id"
+        :name="message.from"
+        :text="[message.text]"
+        :sent="message.from === 'me'"
+      />
+    </div>
+    <q-footer elevated>
+      <q-toolbar class="q-py-sm">
+        <q-input
+          v-model="newMessage"
+          class="full-width"
+          outlined
+          bg-color="white"
+          rounded
+          clearable
+          clear-icon="close"
+          label="Message"
+        >
+          <template v-slot:after>
+            <q-btn
+              rounde
+              dense
+              flat
+              icon="send"
+              color="white"
+              @click="sendMessage"
+            />
+          </template>
+        </q-input>
+      </q-toolbar>
+    </q-footer>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+  data() {
+    return {
+      newMessage: "",
+      messages: [
+        { id: "a01a5s", text: "Hey!", from: "me" },
+        { id: "b5d22", text: "Hi!", from: "them" },
+        { id: "ca321w", text: "Fine", from: "me" },
+      ],
+    };
+  },
+  methods: {
+    sendMessage() {
+      this.messages.push({
+        text: this.newMessage,
+        from: "me",
+      });
+    },
+  },
+};
+</script>
---
+++ src/pages/PageUsers.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
+      <q-item v-for="user in users" :key="user.id" clickable v-ripple to="/chat">
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">{{ user.name.charAt(0) }}</q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge
+            :color="user.online ? 'light-green-5' : 'grey-4'"
+          >{{ user.online ? 'Online' : 'Offline' }}</q-badge>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+const users = [
+  {
+    id: 1,
+    name: "Ruddy Jedrzej",
+    online: true
+  },
+  {
+    id: 2,
+    name: "Mallorie Alessandrini",
+    online: false
+  },
+  {
+    id: 3,
+    name: "Elisabetta Wicklen",
+    online: true
+  }
+];
+
+export default {
+  data() {
+    return {
+      users
+    };
+  }
+};
+</script>
